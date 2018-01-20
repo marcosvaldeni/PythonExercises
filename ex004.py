b9641fdc03a3b5db5cf2c1e57f98d58f394121f1
Marcos Lucas <marcosvaldeni@gmail.com>
2018-01-20 14:52:56 +0000

    exercises 3 and 4 done

---
+++ ex004.py
@@ -0,0 +1,10 @@
+n = input('Type something: ')
+print('The primitive type of this value is:', type(n))
+print('Is it only space?', n.isspace())
+print('Is it a numeric?', n.isnumeric())
+print('Is it alphabetic?', n.isalpha())
+print('Is it a alphanumeric?', n.isalnum())
+print('Is it only uppercase?', n.isupper())
+print('Is it only lowercase?', n.islower())
+print('Is it capitalized?', n.istitle())
+
