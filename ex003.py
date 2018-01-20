528fbc748feb9b0e3d07bc178820bfdbd7bea9c2
Marcos Lucas <marcosvaldeni@gmail.com>
2018-01-20 14:52:56 +0000

    exercises 3 and 4 done!

---
+++ ex003.py
@@ -0,0 +1,4 @@
+number1 = int(input('Type a number: '))
+number2 = int(input('Type another number: '))
+r = number1 + number2
+print('The sum between {0} and {1} is: {2}'.format(number1, number2, r))
