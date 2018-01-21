42b9dfd2aece39a56818da7149ba2558e285c91f
Marcos Lucas <marcosvaldeni@gmail.com>
2018-01-21 23:58:33 +0000

    exercises 14 and 15 done

---
+++ ex014.py
@@ -0,0 +1,2 @@
+celsius = float(input('Report the celsius temperature:'))
+print('The temperature {}ºC is equivalent to {}ºF'.format(celsius, celsius*1.8+32))
---
+++ ex015.py
@@ -0,0 +1,3 @@
+days = int(input('How many days? '))
+km = float(input('How may km? '))
+print('Total amount to pay is {:.2f}'.format((days*60)+(km*0.15)))
